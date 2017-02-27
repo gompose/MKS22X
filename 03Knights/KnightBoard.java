3715dd34c8e207d87fe3e2ade603fa38f6e05896
Peter Mei <peter.m12321@gmail.com>
2017-02-27 12:32:50 -0500

    Fixed stuff

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -15,13 +15,52 @@ public class KnightBoard {
 	    }
 	    boardstate = boardstate + "\n";
 	}
-	return boardstate;	
+	return boardstate;
     }
-    public void solve() {
+    /*public void solve() {
 	solveH(0, 0, 1);
+	}*/
+
+    //private boolean solveH(int row, int col, int level){};
+
+    private boolean addKnight(int x, int y, int number) {
+	if (board[x][y] == 0) {
+	    board[x][y] = number;
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+    private boolean removeKnight(int x, int y) {
+	if (board[x][y] != 0) {
+	    board[x][y] = 0;
+	    return true;
+	} else {
+	    return false;
+	}
     }
 
-    private boolean solveH(int row, int col, int level){};
+    /*
+    private boolean solved() {
+	for(int i = 0; i < board.length; i++) {
+	    for(int n = 0; n < board[i].length; n++) {
+		if (board[i][n] < 10) {
+		    boardstate = boardstate + "_" + board[i][n] + " ";
+		} else {
+		    boardstate = boardstate + board[i][n] + " ";
+		}
+	    }
+	    boardstate = boardstate + "\n";
+	}
+    }
+    */
+	
+
+    public static void main(String[] args) {
+	KnightBoard c = new KnightBoard(7, 7);
+	System.out.println(c);
+    }
+
 
 	
 
