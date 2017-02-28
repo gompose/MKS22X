971f5592ed31040a19ee076a4ccdf319453a0e26
Peter Mei <peter.m12321@gmail.com>
2017-02-28 12:38:12 -0500

    Fiddled with things

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,10 +1,12 @@
 public class KnightBoard {
     private int[][] board;
     private static final int[][] options = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
-    private int[][] validOptions;
+    private int[][] validOptions = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0,0}, {0, 0}, {0, 0}};
+
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
     }
+
     public String toString() {
 	String boardstate = "";
 	for(int i = 0; i < board.length; i++) {
@@ -20,12 +22,12 @@ public class KnightBoard {
 	return boardstate;
     }
     
-    /*public void solve() {
+    public void solve() {
 	solveH(0, 0, 1);
 	}
 
     private boolean solveH(int row, int col, int level){
-	addKnight(row, col, level);
+	/*addKnight(row, col, level);
 	if (solved()) {
 	    return true;
 	} else {
@@ -34,11 +36,25 @@ public class KnightBoard {
 	    //      Backtrack to 1 previous level
 	    //      Recalculate Options
 
-	    // My brain is  like mashed tomatoes right now.
+	    
 	}
-
+	*/
+	addKnight(row, col, level);
+	recalculateMoves(row, col);
+	if (validOptions[0][0] == 0) {
+	    removeKnight(row, col);
+	    return false;
+	}
+	return solveH(validOptions[0][0] + row, validOptions[0][1] + col, level + 1) 
+	    || solveH(validOptions[1][0] + row, validOptions[1][1] + col, level + 1)
+	    || solveH(validOptions[2][0] + row, validOptions[2][1] + col, level + 1)
+	    || solveH(validOptions[3][0] + row, validOptions[3][1] + col, level + 1)
+	    || solveH(validOptions[4][0] + row, validOptions[4][1] + col, level + 1)
+	    || solveH(validOptions[5][0] + row, validOptions[5][1] + col, level + 1)
+	    || solveH(validOptions[6][0] + row, validOptions[6][1] + col, level + 1)
+	    || solveH(validOptions[7][0] + row, validOptions[7][1] + col, level + 1);
     }
-    */
+    
     
     private boolean addKnight(int x, int y, int level) {
 	if (board[x][y] == 0) {
@@ -69,12 +85,14 @@ public class KnightBoard {
 	}
 	return isSolved;
     }
-    /*
+    
     private int openSquares(int row, int col) {
 	int i = 0;
         if (inBounds(row, col, 1) && board[row][col] = 0) {
 	    i++;
 	}
+	return i;
+    }
 
     private boolean inBounds(int row, int col, int option) {
         boolean bounded;
@@ -82,13 +100,51 @@ public class KnightBoard {
 	boolean colbound = options[option][1] + col >= 0 && options[option][0] + col <= board[row].length;
 	bounded = rowbound && colbound;
     }
+
+    private int recalculateMoves(int row, int col) {
+	int possible = 0;
+	if (inBounds(row, col, 0)){
+	    validOptions[possible] = options[0];
+	    possible++;
+	}
+	if (inBounds(row, col, 1)){
+	    validOptions[possible] = options[1];
+	    possible++;
+	}
+	if (inBounds(row, col, 2)){
+	    validOptions[possible] = options[2];
+	    possible++;
+	}
+	if (inBounds(row, col, 3)){
+	    validOptions[possible] = options[3];
+	    possible++;
+	}
+	if (inBounds(row, col, 4)){
+	    validOptions[possible] = options[4];
+	    possible++;
+	}
+	if (inBounds(row, col, 5)){
+	    validOptions[possible] = options[5];
+	    possible++;
+	}
+	if (inBounds(row, col, 6)){
+	    validOptions[possible] = options[6];
+	    possible++;
+	}
+	if (inBounds(row, col, 7)){
+	    validOptions[possible] = options[7];
+	    possible++;
+	}
+	return possible;
+    }
 	    
-    */
     
 	
 
     public static void main(String[] args) {
-	KnightBoard c = new KnightBoard(7, 7);
+	KnightBoard c = new KnightBoard(5, 5);
+	System.out.println(c);
+	c.solve();
 	System.out.println(c);
     }
 
