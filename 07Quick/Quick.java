1162fd4ea6dae02cf10f2c2649b2f7e7fc8bf675
gompose <peter.m12321@gmail.com>
2017-03-23 07:17:22 -0400

    Not-Finished Quick.java
    Quick Select works.
    Quick Sort doesn't

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -2,10 +2,37 @@ import java.util.*;
 
 public class Quick{
 
-    public static int part (int[] data, int start, int end){
-	int[] parted = scanPart3(Arrays.copyToRange(data, start, end), randomWithRange(start, end));
-	paste(data, parted, start);
-	return 1;
+    // public static int[] quickSort(int[]data]);
+    public static int quickSelect(int[] data, int index) {
+	PartitionPair holder = part(data, 0, data.length - 1);
+	int actual = selectRecurse(data, index, holder);
+	return actual;
+    }
+    private static int selectRecurse(int[] data, int index, PartitionPair holder) {
+	if (holder.getLow() <= index && holder.getHigh() >= index) {
+	    return data[holder.getLow() + holder.getHigh() / 2];
+	} else {
+	    if (holder.getLow() > index) {
+		PartitionPair hold1 = part(data, holder.getMin(), holder.getLow());
+		return selectRecurse(hold1.getArray(), index, hold1);
+	    }else{			 
+		PartitionPair hold2 = part(data, holder.getHigh(), holder.getMax());
+		return selectRecurse(hold2.getArray(), index, hold2);
+	    }
+	}
+    }
+
+	       
+
+    public static PartitionPair part (int[] data, int start, int end){
+	int pivot = randomWithRange(start, end);
+	// A random pivot within the starting range;
+	int[] subArray = Arrays.copyOfRange(data, start, end);
+	PartitionPair holder = scanPart3(subArray, pivot);
+	paste(data, holder.getArray(), start);
+	holder.setMin(start);
+	holder.setMax(end);
+	return holder;
 	
     }
     
@@ -24,7 +51,7 @@ public class Quick{
 	// returns how many as an int
 	return i;
     }
-    private static int[] scanPart3(int[] data, int pivot) {
+    private static PartitionPair scanPart3(int[] data, int pivot) {
 	int[] newArray = new int[data.length];
 	int low = 0;
 	int high = data.length - 1;
@@ -51,8 +78,11 @@ public class Quick{
 	for (int index2 = low; index2 <= high; index2++) {
 	    newArray[index2] = mid;
 	}
-	return newArray;
+	PartitionPair info = new PartitionPair(newArray, low, high);
+	return info;
 	// scanPart3 takes an array, and parts it around the pivot index
+	// Returns a PartitionPair class, whose methods are:
+	// getArray() , getLow() , getHigh();
     }
     private static void swap(int[] data, int index1, int index2) {
 	int hold;
@@ -68,19 +98,31 @@ public class Quick{
 	//Source: http://stackoverflow.com/questions/7961788/math-random-explained
     }
     public static int[] paste(int[] mainArray, int[] subArray, int n){
-	int index = n;
-	while (index < subArray.length){
-	    mainArray[index] = subArray[index];
+	int index = 0;
+	int i = n;
+	while (index < subArray.length) {
+	    mainArray[i] = subArray[index];
+	    i++;
 	    index++;
 	}
 	return mainArray;
 	//Pastes subArray on top of mainArray, beginning at index n
+	//and returns the new pasted array
     }
     public static void main(String[] args) {
+	/*
 	int[] test = {6,3,99,40, 9, 2, 5, 8, 3, 19, 20, 39, 91, 9, 9, 9, 9};
 	System.out.println(Arrays.toString(scanPart3(test, 4)));
+	*/
+	int[] arry1 = {5, 5, 5, 5, 5, 5, 5, 5};
+	int[] arry2 = {10, 10, 10};
+	int[] arry3 = {0, 3, 2, 5, 7, 8, 3, 4, 6, 2, 8, 9, 3, 4};
+	int[] arry4 = {0, 1, 2, 3, 4, 5, 6};
+	//System.out.println(Arrays.toString(paste(arry1, arry2, 1)));
+
+	System.out.println(quickSelect(arry4, 2));
+	
     }
     
 }
 	
-    
