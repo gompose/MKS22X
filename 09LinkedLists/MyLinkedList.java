7783664d5be1b66175641e48370044a2d3fa3286
gompose <peter.m12321@gmail.com>
2017-04-20 07:06:08 -0400

    Update MyLinkedList.java

--- 09LinkedLists/MyLinkedList.java
+++ 09LinkedLists/MyLinkedList.java
@@ -15,7 +15,7 @@ public class MyLinkedList {
     public int size () {
 	return size;
     }
-    public void add (int value) {
+    public boolean add (int value) {
 	if (size == 0) {
 	    start = new LNode(value);
 	    end = start;
@@ -27,6 +27,7 @@ public class MyLinkedList {
 	    end = end.next;
 	    size++;
 	}
+	return true;
     }
     public void add (int value, int index) {
 	LNode holder;
@@ -36,6 +37,7 @@ public class MyLinkedList {
         if (index == size - 1) {
 	    add(value);
 	    end = end.next;
+	    size++;
 	} 
 	if (index == 0) {
 	    start.prev = new LNode(value);
@@ -50,13 +52,139 @@ public class MyLinkedList {
 	    holder = current.next;
 	    current.next = new LNode(value, current, holder);
 	    holder.prev = current.next;
+	    size++;
 	}
     }
-
+    private int getNthLNode(int index){
+	int counter = index;
+	LNode current = start;
+	while (counter != 0) {
+	    current = current.next;
+	    counter--;
+	}
+	return current.value;
+    }
+    private void addAfter(LNode location, LNode toBeAdded) {
+	LNode holder;
+	if (size == 0){
+	    add(toBeAdded.value);
+	}else{
+	    if(location == start){
+		add(0, toBeAdded.value);
+	    }else{
+		if(location == end){
+		    add(size - 1, tobeAdded.value);
+		}else{
+		    holder = location.next;
+		    location.next = toBeAdded;
+		    toBeAdded.prev = location;
+		    holder.prev = toBeAdded;
+		    toBeAdded.next = holder;
+		    size++;
+		}
+	    }
+	}
+    }
+    private void remove(LNode target) {
+	LNode holder1, holder2;
+	if (target == start) {
+	    start = start.next;
+	    start.prev = null;
+	    size--;
+	}else{
+	    if (target == end) {
+		end = end.prev;
+		end.next = null;
+		size--;
+	    }else{
+		holder1 = target.prev;
+		holder2 = target.next;
+		holder1.next = holder2;
+		holder2.prev = holder1;
+		size--;
+	    }
+	}
+    }
+	    		    
+    public int get(int index){
+	int counter = index;
+	LNode current = start;
+	while (counter != 0) {
+	    current = current.next;
+	    counter--;
+	}
+        return current.value;
+    }
+    public int set(int index, int value){
+	int counter = index;
+	LNode current = start;
+	int holder = 0;
+	while (counter != 0) {
+	    current = current.next;
+	    counter--;
+	}
+	holder = current.value;
+	current.value = value;
+	return holder;
+    }
+    public int indexOf(int value) {
+	LNode current = start;
+	int currentIndex = 0;
+	int index;
+	while (current != end){
+	    if (current.value == value){
+		index = currentIndex;
+	    }
+	    current = current.next;
+	    currentIndex++;
+	}
+	return index;
+    }
+    public int remove (int index) {
+	int counter = index;
+	LNode current = start;
+	int holder = 0;
+	while (counter != 0){
+	    current = current.next;
+	    counter--;
+	}
+	holder = current.value;
+	remove(current);
+	return holder;
+    }
+	
+	    
+    
     public String toString() {
 	int index = 0;
 	LNode current = start;
 	String newString = "[";
+	String startInfo = "[";
+	String endInfo = "[";
+	if (start.prev == null) {
+	    startInfo = startInfo + "(null)";
+	} else {
+	    startInfo = startInfo + "(" + start.prev.value + ")";
+	}
+	startInfo = startInfo + start.value;
+	if (start.next == null) {
+	    startInfo = startInfo + "(null),";
+	} else {
+	    startInfo = startInfo + "(" + start.next.value + "),";
+	}
+	startInfo += "]";
+	if (end.prev == null) {
+	    endInfo = endInfo + "(null)";
+	} else {
+	    endInfo = endInfo + "(" + end.prev.value + ")";
+	}
+	endInfo = endInfo + end.value;
+	if (end.next == null) {
+	    endInfo = endInfo + "(null),";
+	} else {
+	    endInfo = endInfo + "(" + end.next.value + "),";
+	}
+	endInfo += "]";
 	while (current.next != null) {
 	    if (current.prev == null) {
 		newString = newString + "(null)";
@@ -84,7 +212,12 @@ public class MyLinkedList {
 	    }
 	    current = current.next;
 	newString += "]";
+	newString = newString + "\n" + "Start Node: ";
+	newString += startInfo;
+	newString = newString + "\n" + "End Node: ";
+	newString += endInfo;
 	newString = newString + "\n" + "Size: " + size;
+	
 	return newString;
     }
 
