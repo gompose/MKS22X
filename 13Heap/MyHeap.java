1133683cfbfa681bd26373153e2464b385596eb1
Peter Mei <peter.m12321@gmail.com>
2017-05-04 12:31:28 -0400

    update

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,13 +1,44 @@
 public class MyHeap {
     private int inverter = 1;
-    private String[] heapArray = new String[10];
-    private int endindex = 1;
+    private String[] heapArray;
+    private int endIndex = 1;
+    private int place;
+
     public MyHeap () {	
+	heapArray = new String[10];
     }
     public MyHeap (boolean max) {
+	// if Max == true; heapArray becomes a maxHeapArray;
+	// if Max != true; heapArray becomes a minHeapArray;
 	if (!max) {
 	    inverter = -1;
 	}
+	heapArray = new String[10];
+    }
+    private void expand() {
+	String[] holder = heapArray;
+	String[] newHeapArray = new String[heapArray.length * 2];
+	for (int x = 0; x < holder.length; x++) {
+	    newHeapArray[x] = holder[x];
+	}
+	heapArray = newHeapArray;
     }
 
+    public void add (String s) {
+	int holder = endIndex;
+	if (endIndex > heapArray.length - 1) {
+	    heapArray[endIndex] = s;	    
+	} else {
+	    expand();
+	    heapArray[endIndex] = s;
+	}
+	// Stopped here
+	while (
+	endIndex++;
+
+    }
+	private void pushUp(){};
+	private void pushDown(){};
+     
+	    
 }
